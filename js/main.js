7056f84a95f8eae4ad3e4c386a4a34b8ee825ed5
Ben Lane <Ben.Lane@blackboard.com>
2018-12-28 14:17:55 -0500

    Site updated: 2018-12-28 14:17:54

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
+function toggleMenu(event) {
+    if (event.type === 'click') {
+        document.removeEventListener('touchstart', toggleMenu);
+    } else {
+        document.removeEventListener('click', toggleMenu);
+    }
+    var element = document.getElementById('menu');
+    var isMenuButtonClick = event.target.closest('#menuButton') != null;
+    var isMenuItemClick = event.target.closest('#menu') != null;
+    var menuIsVisible = element.classList.contains('isVisible');
+    if (isMenuButtonClick && !menuIsVisible) {
+        element.classList.add('isVisible');
+    } else if (!isMenuItemClick) {
+        element.classList.remove('isVisible');
+    }
+}
 function setUpClickListener() {
-    document.addEventListener('click', (event) => {
-        var element = document.getElementById('menu');
-        var isMenuButtonClick = event.target.closest('#menuButton') != null;
-        var menuIsVisible = element.classList.contains('isVisible');
-        if (isMenuButtonClick && !menuIsVisible) {
-            element.classList.add('isVisible');
-        } else {
-            element.classList.remove('isVisible');
-        }
-    });
+    document.addEventListener('touchstart', toggleMenu);
+    document.addEventListener('click', toggleMenu);
 }
